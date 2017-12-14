640747b59f4faa95396ec39e9598e950982eefd8
Mark Tripolt <mark@tripolt.de>
2017-12-14 15:13:51 +0100

    added Testsender

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.io.ByteArrayInputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,23 +16,40 @@ public class Main {
             DatagramSocket receiverSocket = new DatagramSocket(port);
             DatagramPacket packet = new DatagramPacket(data, data.length);
             System.out.println("Waiting for packets...");
-            try{
-                while(true){
-
+            try {
+                while (true) {
+                    System.out.println("Start");
                     receiverSocket.receive(packet);
 
+                    DataInputStream in = new DataInputStream(new ByteArrayInputStream(packet.getData()));
                     int c;
-                    ByteArrayInputStream bInput = new ByteArrayInputStream(packet.getData());
 
-                    for(int y = 0 ; y < 1; y++ ) {
-                        while ((c = bInput.read()) != -1) {
-                            System.out.println(c);
-                        }
-                        bInput.reset();
+                    int ack = in.read();
+                    System.out.println("ack " + ack);
+
+                    byte[] checksum = new byte[8];
+
+                    for (int i = 0; i < checksum.length; i++) {
+                        checksum[i] = in.readByte();
+                        System.out.println("checksum " + checksum[i]);
                     }
 
+                    ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+                    buffer.put(checksum);
+                    buffer.flip();
+                    System.out.println("buffer " + buffer.getLong());
+
+
+                    for (int y = 0; y < 1; y++) {
+                        while ((c = in.read()) != -1) {
+                            System.out.println("data " + c);
+                        }
+                        in.reset();
+                    }
+
+                    System.out.println("End");
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
 
             }
 
