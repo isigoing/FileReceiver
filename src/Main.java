a96d73e1f9420aeda579f78de5e2a9b063bf524a
Mark Tripolt <mark@tripolt.de>
2017-12-14 15:54:36 +0100

    refactored both the Receiver and the Testsender to use Threads

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,3 @@
-import java.io.ByteArrayInputStream;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
-import java.nio.ByteBuffer;
 
 public class Main {
     public static void main(String[] args) {
